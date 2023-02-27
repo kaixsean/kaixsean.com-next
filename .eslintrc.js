5c3ab97eb479b484efb27c2ad2f9dc8c092d9402
kaixsean <kxakai@gmail.com>
2023-02-27 01:50:56 +0000

    Add absolute imports & path aliases alias `@/*` to `"src/*`
    Ref for support in Next.js project:
    https://nextjs.org/docs/advanced-features/module-path-aliases
    
    Ref for support in eslint:
    https://www.npmjs.com/package/eslint-import-resolver-alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,4 +23,14 @@ module.exports = {
       ],
     },
   ],
+  settings: {
+    // Support absolute imports
+    // https://www.npmjs.com/package/eslint-import-resolver-alias
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src"]],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
 };
