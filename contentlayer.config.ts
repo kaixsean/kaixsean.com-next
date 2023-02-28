5b55a5353d5267c3b7d5a9419dba93d21c04f5ab
kaixsean <kxakai@gmail.com>
2023-02-28 03:10:59 +0000

    style:Coding style

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,35 +1,35 @@
-import { defineDocumentType, makeSource } from "./src/lib/contentLayerAdapter";
+import { defineDocumentType, makeSource } from './src/lib/contentLayerAdapter';
 
 export const Post = defineDocumentType(() => ({
-  name: "Post",
+  name: 'Post',
   filePathPattern: `content/posts/**/*.md`,
   fields: {
     title: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     description: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     slug: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     date: {
-      type: "date",
+      type: 'date',
       required: true,
     },
   },
   computedFields: {
     path: {
-      type: "string",
+      type: 'string',
       resolve: (post) => `/posts/${post.slug}`,
     },
   },
 }));
 
 export default makeSource({
-  contentDirPath: "content",
+  contentDirPath: 'content',
   documentTypes: [Post],
 });
