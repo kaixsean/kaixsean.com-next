4626c0f697a631b98a24bd363aa745d9139eb84c
kaixsean <kxakai@gmail.com>
2023-02-28 03:55:45 +0000

    style:Coding style

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,7 +3,7 @@ import { defineDocumentType, makeSource } from './src/lib/contentLayerAdapter';
 export const Post = defineDocumentType(() => ({
   name: 'Post',
   filePathPattern: `content/posts/**/*.mdx`,
-  contentType: "mdx",
+  contentType: 'mdx',
   fields: {
     title: {
       type: 'string',
