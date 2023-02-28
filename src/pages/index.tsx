7574af4708e045f618f532372e5272fd5853dc36
kaixsean <kxakai@gmail.com>
2023-02-28 12:40:00 +0000

    feat:Apply basic style to IndexPage & reset style of PostPage Try some basic tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next';
-import { Inter } from 'next/font/google';
 import Head from 'next/head';
-import Image from 'next/image';
 
 import { allPostsNewToOld, Post } from '@/lib/contentLayerAdapter';
-import styles from '@/styles/Home.module.css';
 
 export function getStaticProps() {
   const posts = allPostsNewToOld;
@@ -15,8 +12,6 @@ type Props = {
   posts: Post[];
 };
 
-const inter = Inter({ subsets: ['latin'] });
-
 const Home: NextPage<Props> = ({ posts }) => {
   return (
     <>
@@ -26,115 +21,17 @@ const Home: NextPage<Props> = ({ posts }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main className="p-4">
+        <h1 className="mb-6 text-4xl font-bold">Welcome to my blog!</h1>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-        <div className={styles.grid}>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
           {posts.map((post) => (
-            <a key={post.slug} href={post.path} className={styles.card}>
-              <h2>{post.title}</h2>
-              <p>{post.description}</p>
-            </a>
+            <div key={post.slug} className="rounded-lg border border-black p-6">
+              <a href={post.path}>
+                <h2 className="mb-4 text-2xl font-semibold">{post.title}</h2>
+                <p>{post.description}</p>
+              </a>
+            </div>
           ))}
         </div>
       </main>
