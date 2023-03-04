420d28115890999532f47ef84b49ee91bed698ba
kaixsean <kxakai@gmail.com>
2023-03-04 12:56:21 +0000

    feat:Add CustomLink to posts

--- src/components/CustomLink/CustomLink.tsx
+++ src/components/CustomLink/CustomLink.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import Link from 'next/link';
 
+import ExternalLinkIcon from "./external-link.svg";
+
 type Props = React.ComponentPropsWithoutRef<'a'>;
 
 const CustomLink = ({ href, ...rest }: Props) => {
@@ -19,7 +21,14 @@ const CustomLink = ({ href, ...rest }: Props) => {
     return <a href={href} {...rest} />;
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />;
+  return (
+    <a target="_blank" rel="noopener noreferrer" href={href} {...rest}>
+      {children}
+      {typeof children === "string" && (
+        <ExternalLinkIcon className="ml-1 inline-block h-4 w-4" />
+      )}
+    </a>
+  );
 };
 
 export default CustomLink;
