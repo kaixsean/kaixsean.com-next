77efe07979f2a272478805c602796f4cb281f905
kaixsean <kxakai@gmail.com>
2023-03-04 13:07:05 +0000

    feat:Add post to demo custom link

--- src/components/CustomLink/CustomLink.tsx
+++ src/components/CustomLink/CustomLink.tsx
@@ -5,26 +5,30 @@ import ExternalLinkIcon from "./external-link.svg";
 
 type Props = React.ComponentPropsWithoutRef<'a'>;
 
-const CustomLink = ({ href, ...rest }: Props) => {
+const CustomLink = ({ href, children, ...rest }: Props) => {
   const isInternalLink = href && href.startsWith('/');
   const isAnchorLink = href && href.startsWith('#');
 
   if (isInternalLink) {
     return (
       <Link href={href}>
-        <span {...rest}></span>
+        <span {...rest}>{children}</span>
       </Link>
     );
   }
 
   if (isAnchorLink) {
-    return <a href={href} {...rest} />;
+    return (
+      <a href={href} {...rest}>
+        {children}
+      </a>
+    );
   }
 
   return (
     <a target="_blank" rel="noopener noreferrer" href={href} {...rest}>
       {children}
-      {typeof children === "string" && (
+      {typeof children === 'string' && (
         <ExternalLinkIcon className="ml-1 inline-block h-4 w-4" />
       )}
     </a>
