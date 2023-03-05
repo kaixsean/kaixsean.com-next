78f59d6294417be54f6e78dd224257d59e5221e9
kaixsean <kxakai@gmail.com>
2023-03-05 03:11:14 +0000

    feat:Integrate giscus as comment system under PostPage https://github.com/giscus/giscus https://giscus.app/zh-TW https://github.com/Kamigami55/nextjs-tailwind-contentlayer-blog-starter/discussions

---
+++ src/configs/giscusConfigs.ts
@@ -0,0 +1,6 @@
+export const giscusConfigs = {
+  repo: 'kaixsean/nextjs-typescript' as `${string}/${string}`,
+  repoId: 'R_kgDOJCn94g',
+  category: 'Announcements',
+  categoryId: 'DIC_kwDOJCn94s4CUpt4',
+};
