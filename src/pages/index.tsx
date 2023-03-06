bc77aeb47196f6f59b4e322980379cc04be7ec7d
kaixsean <kxakai@gmail.com>
2023-03-06 13:35:25 +0000

    feat:Add l10n strings to all pages en & zh-TW

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,15 +62,15 @@ const Home: NextPage<Props> = ({ posts, commandPalettePosts }) => {
         />
 
         <div className="prose my-12 space-y-2 transition-colors dark:prose-dark md:prose-lg md:space-y-5">
-          <h1 className="text-center sm:text-left">Hey，I am Iron Man ?</h1>
-          <p>我是 Tony Stark，不是 Stank！</p>
-          <p>老子很有錢，拯救過很多次世界。</p>
-          <p>我討厭外星人、紫色的東西、和紫色外星人。</p>
+          <h1 className="text-center sm:text-left">{t('intro-title')}</h1>
+          <p>{t('intro-1')}</p>
+          <p>{t('intro-2')}</p>
+          <p>{t('intro-3')}</p>
         </div>
 
         <div className="my-4 divide-y divide-gray-200 transition-colors dark:divide-gray-700">
           <div className="prose prose-lg my-8 dark:prose-dark">
-            <h2>最新文章</h2>
+            <h2>{t('latest-posts')}</h2>
           </div>
 
           <PostList posts={posts} />
